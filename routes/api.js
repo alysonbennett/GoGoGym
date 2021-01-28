0e8d908a9a58b6cc004cf97356e09c835651abff
alysonbennett <alysonbennett@gmai.com>
2021-01-27 19:21:07 -0500

    fixed code in server and routes

--- routes/api.js
+++ routes/api.js
@@ -1,54 +1,53 @@
 const router = require("express").Router();
 const db = require("../models")
 
-// module.exports = function(app){ 
-    router.get("/api/workouts", function(req,res){  
-        db.Workout.find({})
-        .then(dbWorkout =>{  
+
+router.get("/api/workouts", function (req, res) {
+    db.Workout.find({})
+        .then(dbWorkout => {
             res.json(dbWorkout)
         })
-        .catch(err => { 
+        .catch(err => {
             res.status(400).json(err);
         })
-    });
+});
 
-    router.post("/api/workouts", function (req,res){    
-        db.Workout.create({})
+router.post("/api/workouts", function (req, res) {
+    db.Workout.create({})
         .then(dbWorkout => res.json(dbWorkout))
-        .catch(err => { 
+        .catch(err => {
             res.status(400).json(err);
         })
-    });
+});
 
-    router.get("/api/workouts/range", function (req,res){  
-        db.Workout.find()
-        .then(dbWorkout =>{  
+router.get("/api/workouts/range", function (req, res) {
+    db.Workout.find()
+        .then(dbWorkout => {
             res.json(dbWorkout)
         })
-        .catch(err => { 
+        .catch(err => {
             res.status(400).json(err);
         })
-    });
+});
 
-    router.post("/api/workouts/range", function (req,res){    
-        db.Workout.create({})
+router.post("/api/workouts/range", function (req, res) {
+    db.Workout.create({})
         .then(dbWorkout => res.json(dbWorkout))
-        .catch(err => { 
+        .catch(err => {
             res.status(400).json(err);
         })
-    });
+});
 
-    router.put("/api/workouts/:id", ({body,params},res) => {   
-        db.Workout.findByIdAndUpdate(  
-         params.id,
-         {$push:{exercises:body} },
-         {new: true,runValidators:true }
-        )
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+    db.Workout.findByIdAndUpdate(
+        params.id,
+        { $push: { exercises: body } },
+        { new: true, runValidators: true }
+    )
         .then(data => res.json(data))
-        .catch(err => { 
+        .catch(err => {
             res.status(400).json(err);
         })
-    });
-// }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -22,7 +22,7 @@ mongoose.connect(
 
 // routes
 app.use(require("./routes/api"));
-app.use(require("./routes/htmlRoutes"));
+require('./routes/htmlRoutes')(app)
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
